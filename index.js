b061ad19333776dfa578f6e831992a5ad4d55052
Adanna-Nnajiofor <adanna.nnajiofor@gmail.com>
2024-03-13 15:27:41 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const inputValue = document.getElementById("value");
+const submit = document.getElementById("submit");
+const result = document.querySelector("#result")
+const refresh = document.getElementById("play-again")
+const form = document.querySelector('.form')
+const numberOfClick = document.querySelector(".number-Of-click");
+let guess = 10;
+let click = 5;
+console.log(guess)
+
+function getValue() {
+    if (inputValue.value == "") {
+        result.innerHTML = "Pless enter a number";
+    }
+    else if (inputValue.value > guess) {
+        result.innerHTML = 'Your guess is high'
+        result.style.color = 'red'
+    }
+    else if (inputValue.value < guess) {
+        result.innerHTML = "Your guess is low"
+        result.style.color = 'blue'
+    }
+    else if (inputValue.value == guess) {
+        result.innerHTML = `You win !!!  Your guess was ${guess}`
+        result.style.color = 'green'
+        refresh.innerHTML = `Click the refresh button to play again`
+        submit.style.display = 'none'
+        reload()
+    }
+    else{
+        return 
+    }
+}
+function clickCounts() {
+    click--;
+    numberOfClick.textContent = click;
+    if (click === 0) {
+        refresh.innerHTML = `Click the refresh button to play again the guess is ${guess}`
+        submit.style.display = 'none'
+        reload()
+        result.style.display = 'none'
+    }
+}
+const reload = () => {
+    const reload = document.createElement('button');
+    reload.textContent = 'refresh'
+    reload.className = 'reload'
+    form.appendChild(reload)
+    reload.addEventListener('click', () => {
+        location.reload()
+    })
+}
+
+submit.addEventListener("click", () => {
+    clickCounts()
+    getValue()
+    inputValue.value = ""
+})
